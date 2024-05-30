186362a322943aef39533e1171676784096d852e
Dwain-Anderson <153378199+DwainAnderson@users.noreply.github.com>
2024-05-30 12:12:12 -0400

    Updated repo URL

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ function Footer()  {
         <h2 className="footer-h2">Contact Information</h2>
             <p className="footer-text">You can reach out to me at <a href="mailto:dka36@cornell.edu">dka36@cornell.edu</a></p>
             <p className="footer-text">This website was made with React, Vite, TypeScript, HTML, and CSS.</p>
-            <a className ="footer-repo"href="https://github.com/Dwain-Anderson/dwain.github.io" target="_blank" rel="noopener noreferrer">Link to this website's repo</a>
+            <a className ="footer-repo"href="https://github.com/Dwain-Anderson/Dwain-Anderson.github.io" target="_blank" rel="noopener noreferrer">Link to this website's repo</a>
     </footer>
     );
 }
