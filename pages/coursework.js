04b7881f49a5ce5062dbbbc5a15cee7717b4ed39
Dwain-Anderson <Dwain-Anderson@users.noreply.github.com>
2026-03-21 21:50:27 -0400

    tweak build error v1

--- pages/coursework.js
+++ pages/coursework.js
@@ -1,5 +1,6 @@
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
+import '../styles/global.css';
 
 const SEMESTERS = [
   {
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 import PortfolioDeckOfCards from '../components/PortfolioDeckOfCards.js';
+import '../styles/global.css';
 
 function Home() {
   return (
